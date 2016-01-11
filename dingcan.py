d2496274bed88e18056b7f6c5e06d89e97f69825
王旭杰 <280597526@qq.com>
2016-01-11 16:03:10 +0800

    1.修复多选bug 2.优化术语 3.完成取消订餐功能

--- dingcan.py
+++ dingcan.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-
 import re
 import requests
 
-url  = "http://dingcan.***.com/index.php?m=login&a=in"
-url1 = "http://dingcan.***.com/index.php"
-url2 = "http://dingcan.***.com/index.php?m=dingcan&a=dinner"
+url      = "http://dingcan.***.com/index.php?m=login&a=in"
+url1     = "http://dingcan.***.com/index.php"
+url2     = "http://dingcan.***.com/index.php?m=dingcan&a=dinner"
 headers  = {"Host":"dingcan.***.com","Referer":"http://dingcan.***.com"}
 s        = requests.Session()
 r        = s.get(url1,headers = headers)
@@ -33,25 +32,27 @@ for item in items1:
 
 #判断是否之前已定过餐
 if(len(sel_cai) > 0):
-    print "\033[36m 已选菜品 \033[0m：",sel_cai
+    print "\033[36m已选菜品\033[0m：",sel_cai
 
 pattern2 = re.compile('<input.*?name="__hash__".*?value="(.*?)" />',re.S)
 __hash__ = re.findall(pattern2,r1.text)
-get_id   = raw_input("选择菜品(填写后面的数字即可,输入0退出订餐,多个请用\033[31m|\033[0m隔开,\033[32;49;1m 每次重定会覆盖之前的,请认真核对 \033[39;49;0m)：")
+get_id   = raw_input("-------------------------------请根据下面提示选择菜品--------------------------------\n(填写后面的数字即可,0退出订餐,c取消订餐,多选请用\033[31m|\033[0m隔开,\033[32;49;1m 每次重定会覆盖之前的,请认真核对 \033[39;49;0m)：")
 if(get_id == "0"):
     print '你已退出订餐'
     exit()
-ids = get_id.split('|')
-for i in ids:
-    #防止用户输入有误，提交错误的数据
-    if(data2.has_key(i)):
-        data2[i] = 1
-
-data2[get_id]      = "1"
+elif(get_id == "c"):
+    #取消之前的订餐,什么都不执行(此处随便加了一句,什么都不写会报错,菜鸟不知道怎么避免这个报错)
+    a = 1 
+else:
+    ids = get_id.split('|')
+    for i in ids:
+        #防止用户输入有误，提交错误的数据
+        if(data2.has_key(i)):
+            data2[i] = 1
 data2["__hash__"]  = __hash__[0]
 data2["do_submit"] = "保存"
 resp2              = s.post(url2,data = data2,headers = headers)#提交订餐内容
 if(resp2.status_code == 200):
-    print '订餐成功'
+    print '保存成功'
 else:
-    print '订餐失败，请重试'
+    print '保存失败，请重试'
